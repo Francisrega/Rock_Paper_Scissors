fd5f6c09db278b7f220a3d3a3b49fd07cdc8bab4
LizC-98 <dev.calderon21@gmail.com>
2020-08-08 15:01:14 -0400

    Game file

---
+++ Game.py
@@ -0,0 +1,37 @@
+class Game:
+  def_init_(self):
+    self.game[]
+
+
+
+def game(player1, player2):
+  if player1 == player2:
+    print("It's a draw. Please try again.\n")
+  elif player1 == "Rock":
+    if player2 == "Scissors":
+      print(first_person + " wins!\n")
+    else:
+      print(second_person + " wins:\n")
+  elif player1 == "Paper":
+    if player2 == "Scissors":
+      print(second_person + " wins!\n")
+    else:
+      print(first_person + " wins!\n")
+  elif player1 == "Scissors":
+    if player2 == "Rock":
+      print(second_person + " wins!\n")
+    else: 
+      print(first_person + " wins!\n")
+def check(player1, player2):
+  if player1 != "Rock" and player1 != "Paper" and player1 != "Scissors":
+    print("You did not enter rock, paper or scissors. Please try again.\n")
+
+  elif player2 != "Rock" and player2 != "Paper" and player2 != "Scissors":
+    print("You did not enter rock, paper or scissors. Please try again.\n")
+while True:
+  player1 = str(input("Rock, Paper or Scissors?\n"))
+  player2 = str(input("Rock, Paper or Scissors?\n"))
+
+  check(player1, player2)
+  game(player1, player2)
+  break
