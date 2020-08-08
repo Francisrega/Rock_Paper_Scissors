4e6a7ebe8786ce68cbe363a44dc94299cf450621
Francisrega <francis.regalado1@gmail.com>
2020-08-08 12:04:52 -0400

    rock paper scissors

--- RPS.py
+++ RPS.py
@@ -4,11 +4,11 @@ first_person = input("First players name: ")
 second_person = input("Second players name: ")
 
 def check(player1, player2):
-  if player1 != "Rock" and player1 != "Paper" and player1 != "Scissors":
-    print("You did not enter rock, paper or scissors. Please try again.\n")
+    if player1 != "Rock" and player1 != "Paper" and player1 != "Scissors":
+        print("You did not enter rock, paper or scissors. Please try again.\n")
 
-  elif player2 != "Rock" and player2 != "Paper" and player2 != "Scissors":
-    print("You did not enter rock, paper or scissors. Please try again.\n")
+    elif player2 != "Rock" and player2 != "Paper" and player2 != "Scissors":
+        print("You did not enter rock, paper or scissors. Please try again.\n")
 def game(player1, player2):
   if player1 == player2:
     print("It's a draw. Please try again.\n")
