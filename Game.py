410142f7f917dcd71df04a15f882d85eeeb8d54a
Raymond Martinez <raymondmartinez@Raymonds-MBP.home>
2020-08-11 19:49:34 -0400

    Bug fixes

--- Game.py
+++ Game.py
@@ -1,6 +1,8 @@
 class Game:
-  def_init_(self):
-    self.game[]
+  def __init__(self):
+    self.player1 = None
+    self.player2 = None
+    self.game = game(player1, player2)
 
 
 
@@ -9,19 +11,19 @@ def game(player1, player2):
     print("It's a draw. Please try again.\n")
   elif player1 == "Rock":
     if player2 == "Scissors":
-      print(first_person + " wins!\n")
+      print(player1 + " wins!\n")
     else:
-      print(second_person + " wins:\n")
+      print(player2 + " wins:\n")
   elif player1 == "Paper":
     if player2 == "Scissors":
-      print(second_person + " wins!\n")
+      print(player2 + " wins!\n")
     else:
-      print(first_person + " wins!\n")
+      print(player1 + " wins!\n")
   elif player1 == "Scissors":
     if player2 == "Rock":
-      print(second_person + " wins!\n")
+      print(player2 + " wins!\n")
     else: 
-      print(first_person + " wins!\n")
+      print(player1 + " wins!\n")
 def check(player1, player2):
   if player1 != "Rock" and player1 != "Paper" and player1 != "Scissors":
     print("You did not enter rock, paper or scissors. Please try again.\n")
